a4675e23545c25eec0214dc131d9a5ee89a742ef
vladimir <vladimirsok420@list.ru>
2022-01-11 07:13:55 +0300

    1 коммит

---
+++ src/skypro/lesson9/Book.java
@@ -0,0 +1,36 @@
+package skypro.lesson9;
+
+public class Book {
+    private final Author author;
+    private final String title;
+    private int publisherYear;
+
+    public Book(Author author, String name, int publisherYear){
+        this.author = author;
+        this.title = name;
+        this.publisherYear = publisherYear;
+    }
+    public Author getAuthor(){
+        return author;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public int getPublisherYear(){
+        return publisherYear;
+    }
+    public void setPublisherYear (int publisherYear) {
+
+        this.publisherYear = publisherYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author=" + author +
+                ", title='" + title + '\'' +
+                ", publisherYear=" + publisherYear +
+                '}';
+    }
+
+}
